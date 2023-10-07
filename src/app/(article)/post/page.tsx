20d3d1db713dbdc949b2eebab09475c7619f2248
rizky.rachmawan <rizky.rachmawan25@gmail.com>
2023-10-07 22:50:02 +0700

    fix: remove unused prefetch

--- src/app/(article)/post/page.tsx
+++ src/app/(article)/post/page.tsx
@@ -3,13 +3,11 @@ import styles from '@styles/article.module.css';
 import React from 'react';
 import RQHydrate from '@components/reactQueryHydrate';
 import ListArticle from '@/fragments/article/listArticle';
-import { getUser } from '@services/user';
 import prefetchingQuery from '@utils/prefetchQuery';
 
 export default async function Post() {
   const hydrateQueries = [
     { key:'articles',fn:getArticle },
-    { key:'users',fn:getUser },
   ]
   const prefetchedQuery = await prefetchingQuery(hydrateQueries)
   return (
