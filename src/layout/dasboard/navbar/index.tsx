872acbd951a70e4be6c1b951701424b1ab229d90
rizky.rachmawan <rizky.rachmawan25@gmail.com>
2023-10-09 10:17:10 +0700

    fix: downgrade next to 13.4.9

--- src/layout/dasboard/navbar/index.tsx
+++ src/layout/dasboard/navbar/index.tsx
@@ -27,7 +27,6 @@ export default function LayoutNavbar(){
       ]
     },
   ]
-  console.log('pathname',pathname)
   return (
     <AppShellNavbar className={styles.navContainer }>
       {menus.map((menu,index)=>{
